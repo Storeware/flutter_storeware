0d02f4c3ea03d268ff75d88710d9bf944b9279ec
amarildolacerAda <amarildo_51@msn.com>
2019-07-23 14:48:24 -0300

    - transferido para package  models.

--- controls/lib/data.dart
+++
@@ -1,6 +0,0 @@
-library data;
-export './data/dataset_client.dart';
-export './data/rest_client.dart';
-export './data/dataset_json.dart';
-export './data/odata.dart';
-export './data/rest_client.dart';

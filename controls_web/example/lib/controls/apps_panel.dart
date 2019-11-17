f745316fdbaa467d568d7dc23e55e651a63980ba
Amarildo Lacerda <amarildo_51@msn.com>
2019-11-17 22:09:02 +0000

    none

--- controls_web/example/lib/controls/apps_panel.dart
+++ controls_web/example/lib/controls/apps_panel.dart
@@ -10,6 +10,8 @@ class AppsGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     int cols = MediaQuery.of(context).size.width ~/ 150;
     if (cols<2) cols = 2;
-    return SliverContents(grid: children ,crossAxisCount: cols , );
+    return SliverContents(grid: children ,
+    
+    crossAxisCount: cols , );
   }
 }

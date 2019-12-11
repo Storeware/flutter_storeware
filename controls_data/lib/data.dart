203d1eba871d824443f2e5719c4a38ff3237c1fc
amarildolacerAda <amarildo_51@msn.com>
2019-12-11 18:05:53 -0300

    + teste unitario

--- controls_data/lib/data.dart
+++ controls_data/lib/data.dart
@@ -3,3 +3,4 @@ library controls_data;
 export 'data_model.dart';
 export 'local_storage.dart';
 export 'rest_client.dart';
+export 'odata_client.dart';
--- controls_data/test/controls_data_test.dart
+++ controls_data/test/controls_data_test.dart
@@ -1,13 +1,53 @@
-//import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-//import 'package:controls_data/data.dart';
+import 'package:controls_data/data.dart';
 
 void main() {
-  /*test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });*/
+  test('tetar ODataClient', () {
+    final o = ODataClient();
+    o.prefix = 'http';
+    expect(o.prefix, 'http');
+  });
+  test('testar LocalStorage', () {
+    final f = LocalStorage();
+    f.setKey('teste', 'ok');
+    expect(f.getKey('teste'), 'ok');
+  });
+  test('testar DataItem', () {
+    var o = TesteItem();
+    o.codigo = '1';
+    expect(o.codigo, '1');
+  });
+  test('testar Model', () {
+    var o = TesteModel().newItem();
+    o.codigo = '1';
+    expect(o.codigo, '1');
+  });
+}
+
+class TesteItem extends DataItem {
+  String codigo;
+  TesteItem();
+  TesteItem.fromJson(json) {
+    fromMap(json);
+  }
+
+  @override
+  fromMap(Map<String, dynamic> json) {
+    codigo = json['codigo'];
+    return this;
+  }
+
+  @override
+  toJson() {
+    Map<String, dynamic> data = {"codigo": codigo};
+    return data; // itens
+  }
+}
+
+class TesteModel extends DataRows<TesteItem> {
+  @override
+  TesteItem newItem() {
+    return TesteItem();
+  }
 }

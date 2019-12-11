af73b674d8e75bec1dea3b854c1d11d682fd9c3d
Amarildo Lacerda <amarildo_51@msn.com>
2019-12-11 13:40:10 +0000

    + responsive.dart

---
+++ controls_web/lib/controls/responsive.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveBuilder extends StatelessWidget {
+  const ResponsiveBuilder({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}

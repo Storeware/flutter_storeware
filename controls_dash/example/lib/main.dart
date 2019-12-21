7e1e13be6337481e2194b70d784f1b68641eaf6a
amarildolacerAda <amarildo_51@msn.com>
2019-12-21 17:27:18 -0300

    + example

--- controls_dash/example/lib/main.dart
+++ controls_dash/example/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'main_view.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -20,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainView(),
     );
   }
 }
---
+++ controls_dash/example/lib/main_view.dart
@@ -0,0 +1,50 @@
+import 'package:controls_dash/controls_dash.dart';
+import 'package:flutter/material.dart';
+
+class MainView extends StatefulWidget {
+  MainView({Key key}) : super(key: key);
+
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('teste')),
+      body: Wrap(
+        children: [
+          Text('teste'),
+          Container(
+            width: 200,
+            height: 200,
+            child: DashBarChart.withSampleData(),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            child: DashPieChart.withSampleData(),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            child: DashDanutChart.withSampleData(),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            child: DashHorizontalBarChart.withSampleData(),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            child: DashGaugeChart.withSampleData(),
+          ),
+        ],
+        //crossAxisCount: 3,
+      ),
+      //body: Container(),
+    );
+  }
+}

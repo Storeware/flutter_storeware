4a867f6e3316d0c9655897c48869fcc83696a00a
Amarildo Lacerda <amarildo_51@msn.com>
2020-02-01 17:57:02 -0300

    = ACTIVITYSUMMARY

--- controls_web/lib/controls/activities.dart
+++ controls_web/lib/controls/activities.dart
@@ -5,8 +5,14 @@ class ActivityPanel extends StatelessWidget {
   final Color color;
   final double height;
   final double width;
+  final double radius;
   const ActivityPanel(
-      {Key key, this.color, this.width, this.height, this.child})
+      {Key key,
+      this.radius = 20,
+      this.color,
+      this.width,
+      this.height,
+      this.child})
       : super(key: key);
 
   @override
@@ -15,7 +21,7 @@ class ActivityPanel extends StatelessWidget {
       height: height,
       width: width,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(radius),
         color: color,
       ),
       child: child,
@@ -292,6 +298,248 @@ class ActivityCard extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+enum ActivitySummaryPosition { none, left, right }
+
+class ActivitySummary extends StatelessWidget {
+  final String title;
+  final double fontSize;
+  final Color color;
+  final Color fontColor;
+  final String value;
+  final Color valueFontColor;
+  final double percent;
+  final int percentDec;
+  final String percentLabel;
+  final Widget image;
+  final double radius;
+  final ActivitySummaryPosition position;
+  const ActivitySummary(
+      {Key key,
+      this.fontSize = 72,
+      this.radius = 15,
+      this.value,
+      this.valueFontColor = Colors.black45,
+      this.position = ActivitySummaryPosition.right,
+      this.percent,
+      this.percentDec = 1,
+      this.percentLabel = '%',
+      this.title,
+      this.image,
+      this.fontColor,
+      this.color})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color cor = color ?? Theme.of(context).primaryColor;
+    Size size = MediaQuery.of(context).size;
+    return ActivityPanel(
+        width: 300,
+        height: 150,
+        radius: radius,
+        color: cor.withAlpha(50),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 32,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(radius),
+                  topRight: Radius.circular(radius),
+                ),
+                color: cor,
+              ),
+              child: Align(
+                child: Text(
+                  title ?? 'titulo somas',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: fontColor ?? Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+                child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                if ((percent != null) &&
+                    (position == ActivitySummaryPosition.left))
+                  buildActivityPercentValue(),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: <Widget>[
+                    Expanded(
+                      child: Center(
+                          child: Text(
+                        value ?? '0000',
+                        style: TextStyle(
+                          fontSize: fontSize,
+                          color: valueFontColor,
+                        ),
+                      )),
+                    ),
+                    ActivityBar(
+                      value: percent,
+                      showValue: false,
+                    ),
+                    SizedBox(
+                      height: 6,
+                    )
+                  ],
+                ),
+                if ((percent != null) &&
+                    (position == ActivitySummaryPosition.right))
+                  buildActivityPercentValue(),
+              ],
+            )),
+          ],
+        ));
+  }
+
+  ActivityPercentValue buildActivityPercentValue() {
+    return ActivityPercentValue(
+        fontColor: valueFontColor,
+        image: image,
+        percentLabel: percentLabel,
+        percent: percent,
+        percentDec: percentDec);
+  }
+}
+
+class ActivityPercentValue extends StatelessWidget {
+  const ActivityPercentValue({
+    Key key,
+    this.fontColor,
+    this.image,
+    this.percentLabel = '%',
+    this.percent,
+    this.percentDec = 1,
+  }) : super(key: key);
+  final Color fontColor;
+  final Widget image;
+  final String percentLabel;
+  final double percent;
+  final int percentDec;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(child: Container(child: image)),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(percentLabel ?? '',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: fontColor,
+                  )),
+              Text(percent.toStringAsFixed(percentDec),
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    color: fontColor,
+                  )),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ActivityBar extends StatelessWidget {
+  final double width;
+  final double height;
+  final double value;
+  final Color color;
+  final Color backgroundColor;
+  final bool showValue;
+  ActivityBar(
+      {Key key,
+      this.value,
+      this.color,
+      this.showValue = true,
+      this.backgroundColor,
+      this.width = 180,
+      this.height = 16})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var w = width * (value / 100);
+    print('$width $w');
+    return Container(
+      padding: EdgeInsets.only(left: 2, right: 2, bottom: 2, top: 2),
+      width: width,
+      height: height,
+
+      //color: Colors.amber,
+      decoration: BoxDecoration(
+        color: backgroundColor ?? theme.backgroundColor,
+        border: Border.all(color: theme.textTheme.overline.color.withAlpha(50)),
+        //shape: BoxShape.circle,
+      ),
+      child: Row(
+        children: <Widget>[
+          Container(
+              width: w,
+              height: height,
+              color: color ?? theme.primaryColor, //Colors.amber,
+              child: Center(
+                child: showValue
+                    ? Text(
+                        '$value %',
+                        style: TextStyle(fontSize: 10),
+                      )
+                    : null,
+              )
+              //color: ,
+              ),
+        ],
+      ),
+    );
+  }
+}
+
+class ActivityTextSection extends StatelessWidget {
+  final Widget appBar;
+  final String title;
+  final String text;
+  final Widget body;
+  final Widget bottom;
+  const ActivityTextSection(
+      {Key key, this.appBar, this.title, this.text, this.body, this.bottom})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size);
+    return Column(
+      children: [
+        if (appBar != null) appBar,
+        if (title != null) Text(title),
+        if (body != null) body,
+        if (text != null) Text(text),
+        if (bottom != null) bottom,
+      ],
     );
   }
 }

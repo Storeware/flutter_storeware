171aca41c8ef6e8d38a72574d0ffa29baf1806d2
Amarildo Lacerda <amarildo_51@msn.com>
2020-02-02 18:58:46 -0300

    +sample

--- controls_web/lib/controls/sidebar.dart
+++ controls_web/lib/controls/sidebar.dart
@@ -2,6 +2,116 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+/*
+/// Sample
+
+return SidebarScaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('visible; ${Sidebar().visible}');
+            Sidebar().switchBar();
+          },
+        ),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      sidebarColor: Colors.grey,
+      sidebarPosition: SidebarPosition.left,
+      sidebarVisible: false,
+      sidebar: SidebarContainer(
+          width: 150,
+          compactWidth: 50,
+          child: Column(
+            children: <Widget>[
+              SidebarHeader(title: 'header'),
+              //Divider(),
+              SidebarButton(
+                  image: Icon(
+                    Icons.add_to_photos,
+                    size: 32,
+                  ),
+                  onPressed: () {
+                    Sidebar().push(Pagina2());
+                  },
+                  //compact: true,
+                  title: 'add photos1'),
+              SidebarButton(
+                  image: Icon(
+                    Icons.home,
+                    size: 32,
+                  ),
+                  onPressed: () {
+                    Sidebar().goHome();
+                  },
+                  title: 'add photos2'),
+              SidebarButton(
+                  image: Icon(
+                    Icons.keyboard_hide,
+                    size: 32,
+                  ),
+                  onPressed: () {
+                    Sidebar().hide();
+                  },
+                  title: 'hide'),
+              SidebarButton(
+                  image: Icon(
+                    Icons.add_to_photos,
+                    size: 32,
+                  ),
+                  title: 'add photos4'),
+              SidebarButton(
+                image: Icon(
+                  Icons.add_to_photos,
+                  size: 32,
+                ),
+                title: 'compact',
+                onPressed: () {
+                  Sidebar().showCompact(compact: !Sidebar.compacted);
+                },
+              ),
+            ],
+          )),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+*/
+
 enum SidebarPosition { none, left, right }
 
 class SidebarScaffold extends StatefulWidget {

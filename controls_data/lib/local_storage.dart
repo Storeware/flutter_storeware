8b3425f0cd3cb069bc769922d78fc4acc2ea0770
amarildolacerAda <amarildo_51@msn.com>
2020-02-15 17:19:25 -0300

    + setJson

--- controls_data/lib/local_storage.dart
+++ controls_data/lib/local_storage.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import "dart:convert";
 import "native_local_storage.dart"
     if (dart.library.html) "html_local_storage.dart"
     if (dart.library.windows) "widows_local_storage.dart";
@@ -6,4 +8,14 @@ class LocalStorage extends PlataformLocalStorage {
   static final _singleton = LocalStorage._create();
   LocalStorage._create();
   factory LocalStorage() => _singleton;
+
+  getJson(String key) {
+    String v = getKey(key) ?? '{}';
+    return jsonDecode(v);
+  }
+
+  setJson(String key, Map<String, dynamic> json) {
+    String v = jsonEncode(json);
+    setKey(key, v);
+  }
 }

34c43e74baccd9471280cd495a97ed7ff8a1b185
amarildolacerAda <amarildo_51@msn.com>
2020-02-26 11:30:15 -0300

    + init()

--- controls_data/control_data_platform_android/lib/control_data_platform_android.dart
+++ controls_data/control_data_platform_android/lib/control_data_platform_android.dart
@@ -1,7 +1,7 @@
 library control_data_platform_android;
 
-import 'dart:convert';
-import 'dart:io';
+//import 'dart:convert';
+//import 'dart:io';
 
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:control_data_platform_interface/control_data_platform_interface.dart';
@@ -9,23 +9,33 @@ import 'package:control_data_platform_interface/control_data_platform_interface.
 class PlatformLocalStorage extends LocalStorageInterface {
   SharedPreferences _prefs;
   init() async {
-    _prefs = await SharedPreferences.getInstance();
+    print('storage.init()');
+    if (_prefs == null) _prefs = await SharedPreferences.getInstance();
   }
 
   @override
   setKey(String key, String value) {
+    init();
     _prefs.setString(key, value);
   }
 
   @override
   String getKey(String key) {
+    init();
     return _prefs.getString(key);
   }
 
   @override
-  bool getBool(key)=>_prefs.getBool(key);
+  bool getBool(key) {
+    init();
+    return _prefs.getBool(key);
+  }
+
   @override
-  setBool(key,value)=>_prefs.setBool(key,value);
+  setBool(key, value) {
+    init();
+    return _prefs.setBool(key, value);
+  }
 
   @override
   dispose() {}

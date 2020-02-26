99e518272c372877ce783fcac494757827532252
amarildolacerAda <amarildo_51@msn.com>
2020-02-26 15:09:05 -0300

    = support universal_io

--- controls_image/controls_image_android/lib/controls_image_android.dart
+++ controls_image/controls_image_android/lib/controls_image_android.dart
@@ -1,24 +1,27 @@
 library controls_image_android;
 
-import 'dart:io';
-
 import 'package:controls_image_interface/controls_image_interface.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:universal_io/prefer_universal/io.dart' as io;
 
 class ControlsImage extends ControlsImageInterface {
   @override
-  Future<File> pickFromGallary({imageQuality}) {
-    return ImagePicker.pickImage(
+  Future<io.File> pickFromGallary({imageQuality}) async {
+    var rt = await ImagePicker.pickImage(
       source: ImageSource.gallery,
       imageQuality: imageQuality,
     );
+    var bytes = await rt.readAsBytes();
+    return io.File.fromRawPath(bytes);
   }
 
   @override
-  Future<File> pickFromCamera({imageQuality}) {
-    return ImagePicker.pickImage(
+  Future<io.File> pickFromCamera({imageQuality}) async {
+    var rt = await ImagePicker.pickImage(
       source: ImageSource.camera,
       imageQuality: imageQuality,
     );
+    var bytes = await rt.readAsBytes();
+    return io.File.fromRawPath(bytes);
   }
 }
--- controls_image/controls_image_web/lib/controls_image_web.dart
+++ controls_image/controls_image_web/lib/controls_image_web.dart
@@ -1,24 +1,24 @@
 library controls_image_web;
 
 import 'dart:typed_data';
-
 import 'package:controls_image_interface/controls_image_interface.dart';
-import 'package:flutter/widgets.dart';
 import 'package:image_picker_web/image_picker_web.dart';
-import 'package:universal_html/prefer_universal/html.dart';
+import 'package:universal_io/prefer_universal/io.dart' as io;
 
 class ControlsImage extends ControlsImageInterface {
   @override
-  Future<File> pickFromGallary({imageQuality}) async {
+  Future<io.File> pickFromGallary({imageQuality}) async {
     Uint8List bytesFromPicker =
         await ImagePickerWeb.getImage(asUint8List: true);
-    return File(bytesFromPicker, 'image.jpg');
+    return io.File.fromRawPath(
+        bytesFromPicker); //File(bytesFromPicker, 'image.jpg');
   }
 
   @override
-  Future<File> pickFromCamera({imageQuality}) async {
+  Future<io.File> pickFromCamera({imageQuality}) async {
     Uint8List bytesFromPicker =
         await ImagePickerWeb.getImage(asUint8List: true);
-    return File(bytesFromPicker, 'image.jpg');
+    return io.File.fromRawPath(
+        bytesFromPicker); //File(bytesFromPicker, 'image.jpg');
   }
 }

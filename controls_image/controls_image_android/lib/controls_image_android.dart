20ac388594507a3c56cdb855cca89bea5cff62c9
amarildolacerAda <amarildo_51@msn.com>
2020-02-26 14:25:20 -0300

    + parameters

--- controls_image/controls_image_android/lib/controls_image_android.dart
+++ controls_image/controls_image_android/lib/controls_image_android.dart
@@ -7,7 +7,7 @@ import 'package:image_picker/image_picker.dart';
 
 class ControlsImage extends ControlsImageInterface {
   @override
-  Future<File> pickFromGallary({imageQuality = 75}) {
+  Future<File> pickFromGallary({imageQuality}) {
     return ImagePicker.pickImage(
       source: ImageSource.gallery,
       imageQuality: imageQuality,
@@ -15,7 +15,7 @@ class ControlsImage extends ControlsImageInterface {
   }
 
   @override
-  pickFromCamera(imageQuality) {
+  Future<File> pickFromCamera({imageQuality}) {
     return ImagePicker.pickImage(
       source: ImageSource.camera,
       imageQuality: imageQuality,

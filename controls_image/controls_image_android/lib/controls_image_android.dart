e99cbe91609e1b136e14982d6e0b935aa8038e92
amarildolacerAda <amarildo_51@msn.com>
2020-02-26 21:17:50 -0300

    = ok - android

--- controls_image/controls_image_android/lib/controls_image_android.dart
+++ controls_image/controls_image_android/lib/controls_image_android.dart
@@ -7,21 +7,17 @@ import 'package:universal_io/prefer_universal/io.dart' as io;
 class ControlsImage extends ControlsImageInterface {
   @override
   Future<io.File> pickFromGallary({imageQuality}) async {
-    var rt = await ImagePicker.pickImage(
+    return await ImagePicker.pickImage(
       source: ImageSource.gallery,
       imageQuality: imageQuality,
     );
-    var bytes = await rt.readAsBytes();
-    return io.File.fromRawPath(bytes);
   }
 
   @override
   Future<io.File> pickFromCamera({imageQuality}) async {
-    var rt = await ImagePicker.pickImage(
+    return await ImagePicker.pickImage(
       source: ImageSource.camera,
       imageQuality: imageQuality,
     );
-    var bytes = await rt.readAsBytes();
-    return io.File.fromRawPath(bytes);
   }
 }

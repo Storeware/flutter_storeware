a29395e54fc0e0a0f66a38bad0c1f2aed559ca43
amarildolacerAda <amarildo_51@msn.com>
2020-03-15 16:45:01 -0300

    + noticetag

--- controls_web/lib/controls/notice_activities.dart
+++ controls_web/lib/controls/notice_activities.dart
@@ -1,5 +1,267 @@
 import 'package:flutter/material.dart';
 
+class NoticeTitle extends StatelessWidget {
+  final String title;
+  final TextStyle style;
+  const NoticeTitle({Key key, this.title, this.style}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8, bottom: 8),
+      child: Text(
+        title,
+        style: style ?? TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+      ),
+    );
+  }
+}
+
+class NoticeTag extends StatelessWidget {
+  final Widget icon;
+  final String text;
+  final Color tagColor;
+  final double border;
+  final double tagWidth;
+  final Function onPressed;
+  final double width;
+  final double height;
+  final double radius;
+  final TextStyle style;
+  const NoticeTag({
+    Key key,
+    this.icon,
+    this.text,
+    this.tagColor,
+    this.onPressed,
+    this.width,
+    this.height = 60,
+    this.tagWidth = 3,
+    this.radius = 5,
+    this.style,
+    this.border = 0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: theme.scaffoldBackgroundColor,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(radius),
+          bottomRight: Radius.circular(radius),
+        ),
+        border: (border > 0)
+            ? Border.all(width: border, color: theme.dividerColor)
+            : Border.symmetric(),
+      ),
+      child: Row(
+        children: <Widget>[
+          Container(
+              width: tagWidth,
+              height: height,
+              color: tagColor ?? theme.primaryColor),
+          Expanded(
+            child: ListTile(
+              onTap: onPressed,
+              leading: icon ?? Icon(Icons.info),
+              title: Center(
+                  child: Text(text,
+                      style: style ??
+                          TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500))),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NoticeText extends StatelessWidget {
+  final String text;
+  final TextStyle style;
+  const NoticeText(this.text, {Key key, this.style}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: style ?? TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+    );
+  }
+}
+
+class NoticeHeader extends StatelessWidget {
+  final Color color;
+  final String title;
+  final double fontSize;
+  final String subtitle;
+  final Color fontColor;
+  const NoticeHeader(
+      {Key key,
+      this.title,
+      this.color,
+      this.fontSize = 24,
+      this.subtitle,
+      this.fontColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NoticeTile(
+      title: title,
+      color: color,
+      fontSize: fontSize,
+      subtitle: subtitle,
+      fontColor: fontColor,
+    );
+  }
+}
+
+class NoticeTile extends StatelessWidget {
+  final Color color;
+  final String title;
+  final double fontSize;
+  final String subtitle;
+  final Color fontColor;
+  final Widget leading;
+  final Widget trailing;
+  final Function onTap;
+  final bool selected;
+
+  NoticeTile(
+      {this.title,
+      Key key,
+      this.fontSize = 18,
+      this.subtitle,
+      this.fontColor,
+      this.color,
+      this.leading,
+      this.trailing,
+      this.selected = false,
+      this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Container(
+      decoration: BoxDecoration(color: color ?? theme.backgroundColor),
+      child: ListTile(
+        leading: leading,
+        trailing: trailing,
+        onTap: onTap,
+        dense: true,
+        selected: selected,
+        title: Text(title ?? '',
+            style: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: fontColor ?? theme.scaffoldBackgroundColor,
+            )),
+        subtitle: Text(subtitle ?? '',
+            style: TextStyle(
+              color: fontColor ?? theme.scaffoldBackgroundColor,
+              fontSize: fontSize * 0.7,
+            )),
+      ),
+    );
+  }
+}
+
+class NoticeButton extends StatelessWidget {
+  final Widget child;
+  final Function onPressed;
+  const NoticeButton({Key key, this.child, this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NoticeBox(
+      child: MaterialButton(
+        padding: EdgeInsets.all(0),
+        child: child,
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
+
+class NoticeInfo extends StatelessWidget {
+  final Widget child;
+  final Widget icon;
+  final double width;
+  final double height;
+  final Color color;
+  final Function onPressed;
+  final double tagWidth;
+  const NoticeInfo(
+      {Key key,
+      this.color,
+      this.child,
+      this.icon,
+      this.width,
+      this.height,
+      this.onPressed,
+      this.tagWidth})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return NoticeBox(
+      color: color,
+      width: width,
+      height: height,
+      child: Container(
+        alignment: Alignment.center,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+                width: 60,
+                padding: EdgeInsets.only(left: 8, right: 8, top: 4),
+                child: InkWell(
+                  child: icon ??
+                      Icon(Icons.info, size: 40, color: theme.backgroundColor),
+                  onTap: onPressed,
+                )),
+            Expanded(child: Center(child: child)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NoticeBox extends StatelessWidget {
+  final Widget child;
+  final double width;
+  final double height;
+  final Color color;
+  const NoticeBox({Key key, this.color, this.child, this.width, this.height})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: theme.dividerColor),
+        color: color ?? theme.primaryColor.withAlpha(50),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: child,
+    );
+  }
+}
+
 class NoticeRow extends StatefulWidget {
   final double width;
   final double height;
@@ -115,12 +377,12 @@ class _NoticeListState extends State<NoticeRow> {
   }
 }
 
-class NoticeTile extends StatelessWidget {
+class NoticeContainer extends StatelessWidget {
   final Color color;
   final Widget child;
   final double width;
   final BorderRadius borderRadius;
-  const NoticeTile(
+  const NoticeContainer(
       {Key key,
       this.child,
       this.color = Colors.blueAccent,
@@ -159,7 +421,7 @@ class NoticeValue extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NoticeTile(
+    return NoticeContainer(
       width: width,
       color: color,
       borderRadius: borderRadius,

a4890e24a0bdc8a15295b9fb5fa4122ecc486c72
amarildolacerAda <amarildo_51@msn.com>
2020-03-17 20:10:00 -0300

    + singleton

--- controls_data/lib/cached.dart
+++ controls_data/lib/cached.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Cached {
+  static final _singleton = Cached._create();
+  factory Cached() => _singleton;
+  Cached._create();
   static final Map<String, Image> _imageCached = {};
   static final Map<String, dynamic> _cached = {};
 
@@ -17,6 +20,7 @@ class Cached {
   static Image image(BuildContext context, String url,
       {Image Function(String) builder}) {
     var item = _imageCached[url];
+    //print(['Cache image:', url, (item != null)]);
     if (item != null) return item;
     item = builder(url);
     if (item != null) _imageCached[url] = item;
@@ -25,6 +29,7 @@ class Cached {
 
   static value<T>(String key, {T Function(String) builder}) {
     var item = _cached[key];
+    //print(['Cache value:', key, (item != null)]);
     if (item != null) return item;
     item = builder(key);
     if (item != null) _cached[key] = item;

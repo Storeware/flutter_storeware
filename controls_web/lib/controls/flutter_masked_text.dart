a78a677c6ce5537e6bb7f5c191d2756f8fd79fc5
amarildolacerAda <amarildo_51@msn.com>
2020-03-24 14:04:02 -0300

    +novo

---
+++ controls_web/lib/controls/flutter_masked_text.dart
@@ -0,0 +1,231 @@
+library flutter_masked_text;
+
+import 'package:flutter/material.dart';
+
+class MaskedTextController extends TextEditingController {
+  MaskedTextController({String text, this.mask, Map<String, RegExp> translator})
+      : super(text: text) {
+    this.translator = translator ?? MaskedTextController.getDefaultTranslator();
+
+    this.addListener(() {
+      var previous = this._lastUpdatedText;
+      if (this.beforeChange(previous, this.text)) {
+        this.updateText(this.text);
+        this.afterChange(previous, this.text);
+      } else {
+        this.updateText(this._lastUpdatedText);
+      }
+    });
+
+    this.updateText(this.text);
+  }
+
+  String mask;
+
+  Map<String, RegExp> translator;
+
+  Function afterChange = (String previous, String next) {};
+  Function beforeChange = (String previous, String next) {
+    return true;
+  };
+
+  String _lastUpdatedText = '';
+
+  void updateText(String text) {
+    if (text != null) {
+      this.text = this._applyMask(this.mask, text);
+    } else {
+      this.text = '';
+    }
+
+    this._lastUpdatedText = this.text;
+  }
+
+  void updateMask(String mask, {bool moveCursorToEnd = true}) {
+    this.mask = mask;
+    this.updateText(this.text);
+
+    if (moveCursorToEnd) {
+      this.moveCursorToEnd();
+    }
+  }
+
+  void moveCursorToEnd() {
+    var text = this._lastUpdatedText;
+    this.selection = new TextSelection.fromPosition(
+        new TextPosition(offset: (text ?? '').length));
+  }
+
+  @override
+  set text(String newText) {
+    if (super.text != newText) {
+      super.text = newText;
+      this.moveCursorToEnd();
+    }
+  }
+
+  static Map<String, RegExp> getDefaultTranslator() {
+    return {
+      'A': new RegExp(r'[A-Za-z]'),
+      '0': new RegExp(r'[0-9]'),
+      '@': new RegExp(r'[A-Za-z0-9]'),
+      '*': new RegExp(r'.*')
+    };
+  }
+
+  String _applyMask(String mask, String value) {
+    String result = '';
+
+    var maskCharIndex = 0;
+    var valueCharIndex = 0;
+
+    while (true) {
+      // if mask is ended, break.
+      if (maskCharIndex == mask.length) {
+        break;
+      }
+
+      // if value is ended, break.
+      if (valueCharIndex == value.length) {
+        break;
+      }
+
+      var maskChar = mask[maskCharIndex];
+      var valueChar = value[valueCharIndex];
+
+      // value equals mask, just set
+      if (maskChar == valueChar) {
+        result += maskChar;
+        valueCharIndex += 1;
+        maskCharIndex += 1;
+        continue;
+      }
+
+      // apply translator if match
+      if (this.translator.containsKey(maskChar)) {
+        if (this.translator[maskChar].hasMatch(valueChar)) {
+          result += valueChar;
+          maskCharIndex += 1;
+        }
+
+        valueCharIndex += 1;
+        continue;
+      }
+
+      // not masked value, fixed char on mask
+      result += maskChar;
+      maskCharIndex += 1;
+      continue;
+    }
+
+    return result;
+  }
+}
+
+///
+/// Mask for monetary values.
+///
+class MoneyMaskedTextController extends TextEditingController {
+  MoneyMaskedTextController(
+      {double initialValue = 0.0,
+      this.decimalSeparator = ',',
+      this.thousandSeparator = '.',
+      this.rightSymbol = '',
+      this.leftSymbol = '',
+      this.precision = 2}) {
+    _validateConfig();
+
+    this.addListener(() {
+      this.updateValue(this.numberValue);
+      this.afterChange(this.text, this.numberValue);
+    });
+
+    this.updateValue(initialValue);
+  }
+
+  final String decimalSeparator;
+  final String thousandSeparator;
+  final String rightSymbol;
+  final String leftSymbol;
+  final int precision;
+
+  Function afterChange = (String maskedValue, double rawValue) {};
+
+  double _lastValue = 0.0;
+
+  void updateValue(double value) {
+    double valueToUse = value;
+
+    if (value.toStringAsFixed(0).length > 12) {
+      valueToUse = _lastValue;
+    } else {
+      _lastValue = value;
+    }
+
+    String masked = this._applyMask(valueToUse);
+
+    if (rightSymbol.length > 0) {
+      masked += rightSymbol;
+    }
+
+    if (leftSymbol.length > 0) {
+      masked = leftSymbol + masked;
+    }
+
+    if (masked != this.text) {
+      this.text = masked;
+
+      var cursorPosition = super.text.length - this.rightSymbol.length;
+      this.selection = new TextSelection.fromPosition(
+          new TextPosition(offset: cursorPosition));
+    }
+  }
+
+  double get numberValue {
+    List<String> parts =
+        _getOnlyNumbers(this.text).split('').toList(growable: true);
+
+    parts.insert(parts.length - precision, '.');
+
+    return double.parse(parts.join());
+  }
+
+  _validateConfig() {
+    bool rightSymbolHasNumbers = _getOnlyNumbers(this.rightSymbol).length > 0;
+
+    if (rightSymbolHasNumbers) {
+      throw new ArgumentError("rightSymbol must not have numbers.");
+    }
+  }
+
+  String _getOnlyNumbers(String text) {
+    String cleanedText = text;
+
+    var onlyNumbersRegex = new RegExp(r'[^\d]');
+
+    cleanedText = cleanedText.replaceAll(onlyNumbersRegex, '');
+
+    return cleanedText;
+  }
+
+  String _applyMask(double value) {
+    List<String> textRepresentation = value
+        .toStringAsFixed(precision)
+        .replaceAll('.', '')
+        .split('')
+        .reversed
+        .toList(growable: true);
+
+    textRepresentation.insert(precision, decimalSeparator);
+
+    for (var i = precision + 4; true; i = i + 4) {
+      if (textRepresentation.length > i) {
+        textRepresentation.insert(i, thousandSeparator);
+      } else {
+        break;
+      }
+    }
+
+    return textRepresentation.reversed.join('');
+  }
+}

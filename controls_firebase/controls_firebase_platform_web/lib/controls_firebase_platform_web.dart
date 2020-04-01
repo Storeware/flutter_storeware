b6a657077fb138da67bbc81689acec22487ea366
amarildolacerAda <amarildo_51@msn.com>
2020-04-01 19:24:04 -0300

    -+ atualizado versao packages.

--- controls_firebase/controls_firebase_platform_web/lib/controls_firebase_platform_web.dart
+++ controls_firebase/controls_firebase_platform_web/lib/controls_firebase_platform_web.dart
@@ -1,25 +1,31 @@
 library controls_firebase_platform_web;
 
-import 'package:cloud_firestore/cloud_firestore.dart' as fs;
+import 'package:cloud_firestore/cloud_firestore.dart' as cf;
 import 'package:controls_firebase_platform_interface/controls_firebase_platform_interface.dart';
-import 'package:firebase_web/firebase.dart' as fb;
+import 'package:firebase_core/firebase_core.dart' as fc;
+import 'package:firebase_storage/firebase_storage.dart' as fs;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+fc.FirebaseApp _app;
+String _gs;
 
 abstract class FirebaseAppDriver extends FirebaseAppDriverInterface {
   FirebaseAppDriver() {
     platform = FirebasePlatform.webbrowser;
   }
-  static fb.App app;
   @override
-  init(options) {
-    app = fb.initializeApp(
-      authDomain: options['authDomain'],
-      storageBucket: options['storageBucket'],
-      apiKey: options['apiKey'],
-      databaseURL: options['databaseURL'],
-      projectId: options['projectId'],
-      messagingSenderId: options['messagingSenderId'],
-      name: options['name'],
+  init(o) async {
+    _gs = 'gs://' + o['storageBucket'];
+    var options = fc.FirebaseOptions(
+      apiKey: o['apiKey'],
+      projectID: o['projectId'],
+      databaseURL: o['databaseURL'],
+      storageBucket: o['storageBucket'],
+      googleAppID: o["appId"],
+      gcmSenderID: o["messagingSenderId"],
     );
+    _app = await fc.FirebaseApp.configure(name: 'DEFAULT', options: options);
   }
 }
 
@@ -27,18 +33,19 @@ abstract class FirestoreDriver extends FirestoreDriverInterface {
   FirestoreDriver();
   @override
   collection(String path) {
-    return fs.Firestore.instance.collection(path);
+    return cf.Firestore.instance.collection(path);
   }
 }
 
 abstract class FirebaseStorageDriver extends FirebaseStorageDriverInterface {
   FirebaseStorageDriver();
+  final fs.FirebaseStorage storage =
+      fs.FirebaseStorage(app: _app, storageBucket: _gs);
   @override
   Future<String> getDownloadURL(String path) async {
     //print('ref: $path');
-    fb.StorageReference firebaseStorageRef = fb.storage().ref(path);
+    fs.StorageReference firebaseStorageRef = this.storage.ref().child(path);
     try {
-      //print('carregou: $path');
       return firebaseStorageRef.getDownloadURL().then((x) {
         //print('Retorno: $x');
         return x.toString();
@@ -51,64 +58,62 @@ abstract class FirebaseStorageDriver extends FirebaseStorageDriverInterface {
 
   @override
   Future<int> uploadFileImage(String path, rawPath, {metadata}) async {
-    fb.StorageReference firebaseStorageRef =
-        fb.storage(FirebaseAppDriver.app).ref(path);
+    fs.StorageReference firebaseStorageRef = storage.ref().child(path);
     //print('$path:$rawPath');
 
-    fb.UploadMetadata md;
-    md = fb.UploadMetadata(
+    fs.StorageMetadata md = fs.StorageMetadata(
         cacheControl: "Public, max-age=12345", customMetadata: metadata ?? {});
-
-    fb.UploadTask uploadTask = firebaseStorageRef.put(rawPath, md);
-    fb.UploadTaskSnapshot taskSnapshot = uploadTask.snapshot;
+    fs.StorageUploadTask uploadTask = firebaseStorageRef.putData(rawPath, md);
+    fs.StorageTaskSnapshot taskSnapshot = uploadTask.lastSnapshot;
     return taskSnapshot.bytesTransferred;
   }
 }
 
 abstract class FirebaseAuthDriver extends FirebaseAuthDriverInterface {
   FirebaseAuthDriver();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
   @override
   createLoginByEmail(email, senha) {
-    return fb.auth().createUserWithEmailAndPassword(email, senha);
+    return _auth.createUserWithEmailAndPassword(email: email, password: senha);
   }
 
-  fb.User currentUser;
+  UserInfo currentUser;
   @override
   isSignedIn() {
     return currentUser != null;
   }
 
   @override
-  fb.User getCurrentUser() {
-    return currentUser;
+  Future<FirebaseUser> getCurrentUser() async {
+    return _auth.currentUser();
   }
 
   @override
   logout() {
-    return fb.auth().signOut();
+    return _auth.signOut();
   }
 
   @override
-  Future<fb.UserCredential> signInAnonymously() async {
-    //print('signInAnonymously()');
-    return fb.auth().signInAnonymously().then((user) {
-      //print('${user.user}');
+  Future<FirebaseUser> signInAnonymously() async {
+    return _auth.signInAnonymously().then((user) {
       this.currentUser = user.user;
-      return user;
+      return user.user;
     });
   }
 
   @override
   signInWithEmail(String email, String senha) {
-    return fb.auth().signInWithEmailAndPassword(email, senha);
+    return _auth.signInWithEmailAndPassword(email: email, password: senha);
   }
 
   @override
   signInWithGoogle() {
-    // TODO: implement signInWithGoogle
-    throw UnimplementedError();
+    return _googleSignIn.signIn();
   }
 
   @override
-  signOutGoogle() {}
+  signOutGoogle() {
+    _googleSignIn.signOut();
+  }
 }

24675e1cf18652563b849b33f2f085ccb0667a8e
amarildolacerAda <amarildo_51@msn.com>
2020-04-24 18:53:14 -0300

    + dynamic extensions

--- controls_extensions/lib/extensions.dart
+++ controls_extensions/lib/extensions.dart
@@ -4,3 +4,37 @@ export 'src/string_extensions.dart';
 export 'src/double_extensions.dart';
 export 'src/datetime_extensions.dart';
 export 'src/object_extensions.dart';
+
+extension DynamicExtension on dynamic {
+  String toStr(value, {def = ''}) {
+    return (value ?? def).toString();
+  }
+
+  int toInt(value, {def = 0}) {
+    if (value is int) return value;
+    if (value is num) return value.toInt();
+    if (value is String) return int.tryParse(value);
+    return def;
+  }
+
+  double toDouble(value, {def = 0.0}) {
+    if (value is double) return value;
+    if (value is int) return value + 0.0;
+    if (value is num) return value + 0.0;
+    if (value is String) return double.tryParse(value);
+    return def;
+  }
+
+  bool toBool(value, {def: false}) {
+    if (value is bool) return value;
+    if (value is num) return (value == 0) ? false : true;
+    if (value is String) return (value == '1' || value == 'T');
+    return def;
+  }
+
+  DateTime toDateTime(value, {DateTime def}) {
+    if (value is String) return DateTime.tryParse(value);
+    if (value is DateTime) return value;
+    return def ?? DateTime.now();
+  }
+}

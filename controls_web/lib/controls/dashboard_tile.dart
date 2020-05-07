8e13b6b2545c6594a424622b423e91187ef81a18
amarildolacerAda <amarildo_51@msn.com>
2020-05-06 22:16:21 -0300

    + color

--- controls_web/lib/controls/dashboard_tile.dart
+++ controls_web/lib/controls/dashboard_tile.dart
@@ -7,23 +7,27 @@ class DashboardTile extends StatelessWidget {
   final Widget image;
   final String title;
   final double width;
+  final Color color;
   const DashboardTile(
       {Key key,
       this.value,
       this.body,
       this.image,
       this.title,
+      this.color,
       this.width = 200})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    Color _textColor = theme.primaryTextTheme.bodyText1.color;
     return Padding(
       padding: const EdgeInsets.all(2.0),
       child: ApplienceTile(
-        color: Colors.blue,
-        titleStyle: TextStyle(color: Colors.white, fontSize: 22),
-        valueStyle: TextStyle(color: Colors.white, fontSize: 32),
+        color: color ?? theme.appBarTheme.color,
+        titleStyle: TextStyle(color: _textColor, fontSize: 18),
+        valueStyle: TextStyle(color: _textColor, fontSize: 28),
         width: width,
         value: value,
         body: body,

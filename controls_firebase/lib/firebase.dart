166e45e7865ac9e8c8af64be26e0d43685e193bf
amarildolacerAda <amarildo_51@msn.com>
2020-05-28 17:48:24 -0300

    -+ firebase_messaging.dart

--- controls_firebase/lib/firebase.dart
+++ controls_firebase/lib/firebase.dart
@@ -1,7 +1,12 @@
 library controls_firebase;
 
+import "package:controls_firebase_platform_android/firebase_messaging.dart" 
+    if (dart.library.js) "package:controls_firebase_platform_web/firebase_messaging.dart" ;
+
+
 export 'firebase_config.dart';
 //export 'firebase_firestore.dart';
 export 'firebase_functions.dart';
 export 'firestorage_images.dart';
 export 'firebase_data_model.dart';
+export 'firebase_messaging.dart';

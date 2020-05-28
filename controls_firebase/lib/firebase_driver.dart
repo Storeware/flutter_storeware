8430a0b8bac423e04db34ae49beffd599e09a47f
amarildolacerAda <amarildo_51@msn.com>
2020-05-28 17:45:12 -0300

    =

--- controls_firebase/lib/firebase_driver.dart
+++ controls_firebase/lib/firebase_driver.dart
@@ -3,6 +3,12 @@ library controls_firebase;
 import "package:controls_firebase_platform_android/controls_firebase_platform_android.dart"
     if (dart.library.js) "package:controls_firebase_platform_web/controls_firebase_platform_web.dart";
 
+import "package:controls_firebase_platform_android/firebase_messaging.dart" 
+    if (dart.library.js) "package:controls_firebase_platform_web/firebase_messaging.dart" ;
+
+export "firebase_messaging.dart";
+
+
 class FirebaseApp extends FirebaseAppDriver {
   static final FirebaseApp _singleton = FirebaseApp._create();
   int callStack = 0;

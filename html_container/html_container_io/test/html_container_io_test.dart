ba499c71a9000138f5ccac7dda44d87033c43598
amarildolacerAda <amarildo_51@msn.com>
2020-06-14 14:29:24 -0300

    + new

---
+++ html_container/html_container_io/test/html_container_io_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:html_container_io/html_container_io.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}

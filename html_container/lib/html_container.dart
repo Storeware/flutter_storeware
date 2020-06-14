c60f0c21a878328340d69875f0a8274c62f3e367
amarildolacerAda <amarildo_51@msn.com>
2020-06-14 14:38:13 -0300

    *2

--- html_container/lib/html_container.dart
+++ html_container/lib/html_container.dart
@@ -1,2 +1,5 @@
 library html_container;
-export 'src/container.dart';
+
+//import "package:html_container_io/html_container_io.dart"
+//    if (dart.library.js)
+//import "package:html_container_web/html_container_web.dart";

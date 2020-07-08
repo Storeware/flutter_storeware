d67b3ee8c6f3d40e7466e74123c75a521986d3fb
amarildolacerda <amarildo_51@msn.com>
2020-07-07 21:29:32 -0400

    + odata

---
+++ controls_data/lib/odata_firestore.dart
@@ -0,0 +1,20 @@
+import 'package:controls_data/odata_client.dart';
+
+class CloudV3 {
+  static final _singleton = CloudV3._create();
+  final client = ODataClient();
+  CloudV3._create() {
+    client.client.connectionTimeout = 15000;
+    client.baseUrl = 'https://us-central1-selfandpay.cloudfunctions.net';
+    client.prefix = '/v3/';
+  }
+  set loja(x) {
+    client.client.addHeader('contaid', x);
+    client.prefix = '/v3/';
+  }
+
+  factory CloudV3() => _singleton;
+  send(ODataQuery query) {
+    return client.send(query);
+  }
+}

895d099d7ee5d819dadae7723ec3c3c96e4964dc
amarildolacerda <amarildo_51@msn.com>
2020-07-12 18:07:15 -0400

    + linux/windows

---
+++ controls_firebase/controls_firebase_windows/test/controls_firebase_windows_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:controls_firebase_windows/controls_firebase_windows.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  });
+}

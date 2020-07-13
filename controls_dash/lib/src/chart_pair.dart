5456477933031909a462b9466a5c04e5fca3ac03
Amarildo Lacerda <amarildo_51@msn.com>
2020-07-12 21:50:47 -0400

    * num

--- controls_dash/lib/src/chart_pair.dart
+++ controls_dash/lib/src/chart_pair.dart
@@ -3,21 +3,21 @@ import 'package:charts_flutter/flutter.dart' as charts;
 /// Sample linear data type.
 class ChartPair {
   final String title;
-  final double value;
+  final num value;
   final charts.Color color;
   ChartPair(this.title, this.value, {this.color});
 }
 
 class ChartPairInt {
   final int title;
-  final double value;
+  final num value;
   final charts.Color color;
   ChartPairInt(this.title, this.value, {this.color});
 }
 
 class ChartPairDouble {
   final double title;
-  final double value;
+  final num value;
   final charts.Color color;
   ChartPairDouble(this.title, this.value, {this.color});
 }

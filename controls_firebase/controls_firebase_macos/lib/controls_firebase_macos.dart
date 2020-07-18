3e05e1e08ddbab553c208999be6459dd9070dae3
User <user@Users-MacBook-Air.local>
2020-07-18 20:04:36 -0300

    = macos

--- controls_firebase/controls_firebase_macos/lib/controls_firebase_macos.dart
+++ controls_firebase/controls_firebase_macos/lib/controls_firebase_macos.dart
@@ -1,7 +1,95 @@
 library controls_firebase_macos;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:controls_firebase_platform_interface/controls_firebase_platform_interface.dart';
+
+class FirebaseAppDriver extends FirebaseAppDriverInterface {
+  FirebaseAppDriver() {
+    platform = FirebasePlatform.linux;
+  }
+
+  @override
+  auth() {
+    // TODO: implement auth
+    //throw UnimplementedError();
+  }
+
+  @override
+  firestore() {
+    // TODO: implement firestore
+    //throw UnimplementedError();
+  }
+
+  @override
+  init(options) {
+    // TODO: implement init
+    //throw UnimplementedError();
+  }
+
+  @override
+  storage() {
+    // TODO: implement storage
+    //throw UnimplementedError();
+  }
+}
+
+class FirebaseFirestoreDriver extends FirestoreDriverInterface {}
+
+class FirebaseStorageDriver extends FirebaseStorageDriverInterface {
+  @override
+  Future<String> getDownloadURL(String path) {
+    // TODO: implement getDownloadURL
+    //throw UnimplementedError();
+  }
+
+  @override
+  // ignore: missing_return
+  Future<int> uploadFileImage(String path, bytes,
+      {Map<String, String> metadata}) async {
+    // TODO: implement uploadFileImage
+    //throw UnimplementedError();
+  }
+}
+
+class FirebaseAuthDriver extends FirebaseAuthDriverInterface {
+  @override
+  createLoginByEmail(email, senha) {
+    // TODO: implement createLoginByEmail
+    //throw UnimplementedError();
+  }
+
+  @override
+  isSignedIn() {
+    // TODO: implement isSignedIn
+    //throw UnimplementedError();
+  }
+
+  @override
+  logout() {
+    // TODO: implement logout
+    //throw UnimplementedError();
+  }
+
+  @override
+  signInAnonymously() {
+    // TODO: implement signInAnonymously
+    //throw UnimplementedError();
+  }
+
+  @override
+  signInWithEmail(String email, String senha) {
+    // TODO: implement signInWithEmail
+    //throw UnimplementedError();
+  }
+
+  @override
+  signInWithGoogle() {
+    // TODO: implement signInWithGoogle
+    //throw UnimplementedError();
+  }
+
+  @override
+  signOutGoogle() {
+    // TODO: implement signOutGoogle
+    //throw UnimplementedError();
+  }
 }

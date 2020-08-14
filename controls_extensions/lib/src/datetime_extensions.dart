ba7e624c07769a055fed0078733beced9bae279c
Amarildo Lacerda <amarildo_51@msn.com>
2020-08-13 21:55:53 -0300

    Update datetime_extensions.dart

--- controls_extensions/lib/src/datetime_extensions.dart
+++ controls_extensions/lib/src/datetime_extensions.dart
@@ -1,234 +1,47 @@
-import 'package:intl/intl.dart';
-import 'time_ago.dart';
+library extensions;
 
-extension DateTimeExtension on DateTime {
-  /// Format DateTime to custom mask
-  format(String dateMask, [lang = 'pt-BR']) {
-    var formatter = new DateFormat(dateMask, lang);
-    return formatter.format(this);
+export 'src/string_extensions.dart';
+export 'src/double_extensions.dart';
+export 'src/datetime_extensions.dart';
+export 'src/object_extensions.dart';
+export 'src/color_extensions.dart';
+
+extension DynamicExtension on dynamic {
+  String toStr(value, {def = ''}) {
+    return (value ?? def).toString();
   }
 
-  e(f) {
-    return this.format('E').substring(0, 1).toUpperCase() +
-        this.format(f ?? '');
+  int toInt(value, {def = 0}) {
+    if (value is int) return value;
+    if (value is num) return value.toInt();
+    if (value is String) return int.tryParse(value);
+    return def;
   }
 
-  dateFormat(String mask, [lang = 'pt-BR']) => DateFormat(mask, lang);
-
-  toTimeString() => DateFormat.Hms().format(this);
-  toDateString() => DateFormat.yMd().format(this);
-
-  DateTime addDays(int value) {
-    return add(Duration(days: value));
+  double toDouble(value, {def = 0.0}) {
+    if (value is double) return value;
+    if (value is int) return value + 0.0;
+    if (value is num) return value + 0.0;
+    if (value is String) return double.tryParse(value);
+    return def;
   }
 
-  DateTime addHours(int value) => add(Duration(hours: value));
-  DateTime addMinutes(int value) => add(Duration(minutes: value));
-  DateTime addMonths(int value) =>
-      DateTime(year, month + value, day, this.hour, this.minute, this.second);
-  DateTime addYears(int value) => DateTime(this.year + value, this.month,
-      this.day, this.hour, this.minute, this.second);
-
-  bool isLeapYear() {
-    int value = this.year;
-    return value % 400 == 0 || (value % 4 == 0 && value % 100 != 0);
+  bool toBool(value, {def: false}) {
+    if (value is bool) return value;
+    if (value is num) return (value == 0) ? false : true;
+    if (value is String) return (value == '1' || value == 'T');
+    return def;
   }
 
-  bool between(DateTime a, DateTime b) {
-    var t = this.toDateTimeSql();
-    return (t.compareTo(a.toDateTimeSql()) == 1) &&
-        (t.compareTo(b.toDateTimeSql()) == -1);
-  }
-
-  String timeAgo({String lang}) {
-    int timeStamp = this.millisecondsSinceEpoch;
-    lang = lang ?? Intl.defaultLocale;
-    Language lg = Language.ENGLISH;
-    if (lang.indexOf('pt') >= 0) lg = Language.PORTUGUESE;
-    return TimeAgo.getTimeAgo(timeStamp, language: lg);
-  }
-
-  DateTime toDate() {
-    return DateTime(this.year, this.month, this.day);
-  }
-
-  DateTime toTime() {
-    return DateTime(0, 0, 0, this.hour, this.minute, this.second);
-  }
-
-  String toTimeSql() {
-    return this.toIso8601String().substring(11, 19);
-  }
-
-  String toDateTimeSql() {
-    return this.toIso8601String().substring(0, 19).replaceAll('T', ' ');
-  }
-
-  String toDateSql() {
-    return this.toIso8601String().substring(0, 10);
-  }
-
-  DateTime endOfHour([int hour]) {
-    return DateTime(this.year, this.month, this.day, hour ?? this.hour, 59, 0);
-  }
-
-  DateTime startOfHour([int hour]) {
-    return DateTime(this.year, this.month, this.day, hour ?? this.hour, 0, 0);
-  }
-
-  DateTime startOfDay() {
-    return DateTime(this.year, this.month, this.day);
-  }
-
-  DateTime endOfDay() {
-    return DateTime(this.year, this.month, this.day, 23, 59, 00);
-  }
-
-  ///  Obtem o primeiro dia da semana {first indica o primeiro dia da semana}
-  DateTime startOfWeek({first = 1}) {
-    return this.add(Duration(days: -(this.weekday - first)));
-  }
-
-  /// Obtem o ultimo dia da semana
-  DateTime endOfWeek({first = 1}) {
-    return this.add(Duration(days: (6 + first) - this.weekday));
-  }
-
-  DateTime startOfMonth() {
-    return DateTime(this.year, this.month, 1);
-  }
-
-  double get hours => this.hour + ((this.minute / 60));
-
-  DateTime endOfMonth() {
-    return DateTime(this.year, this.month + 1, 0);
-  }
-
-  int lastDayOfMonth() {
-    return endOfMonth().day;
-  }
-
-  DateTime startOfYear() {
-    return DateTime(this.year, 1, 1);
-  }
-
-  DateTime endOfYear() {
-    return DateTime(this.year, 12, 31);
-  }
-
-  DateTime toIso8601StringDate({format = 'yyyy-MM-dd'}) {
-    return this.format(format);
-  }
-
-  DateTime dayBefore() {
-    return this.add(Duration(days: -1));
-  }
-
-  DateTime dayAfter() {
-    return this.add(Duration(days: 1));
-  }
-
-  static DateTime yesterday() {
-    return today().addDays(-1);
-  }
-
-  static DateTime tomorrow() {
-    return today().addDays(1);
-  }
-
-  static DateTime today() {
-    DateTime d = DateTime.now();
-    return DateTime(d.year, d.month, d.day);
-  }
-}
-
-/// credits: https://github.com/jogboms/time.dart
-extension NumTimeExtension<T extends num> on T {
-  /// Returns a Duration represented in weeks
-  Duration get weeks => days * DurationTimeExtension.daysPerWeek;
-
-  /// Returns a Duration represented in days
-  Duration get days => milliseconds * Duration.millisecondsPerDay;
-
-  /// Returns a Duration represented in hours
-  Duration get hours => milliseconds * Duration.millisecondsPerHour;
-
-  /// Returns a Duration represented in minutes
-  Duration get minutes => milliseconds * Duration.millisecondsPerMinute;
-
-  /// Returns a Duration represented in seconds
-  Duration get seconds => milliseconds * Duration.millisecondsPerSecond;
-
-  /// Returns a Duration represented in milliseconds
-  Duration get milliseconds => Duration(
-      microseconds: (this * Duration.microsecondsPerMillisecond).toInt());
-
-  /// Returns a Duration represented in microseconds
-  Duration get microseconds =>
-      milliseconds ~/ Duration.microsecondsPerMillisecond;
-
-  /// Returns a Duration represented in nanoseconds
-  Duration get nanoseconds =>
-      microseconds ~/ DurationTimeExtension.nanosecondsPerMicrosecond;
-}
-
-extension DateTimeTimeExtension on DateTime {
-  /// Adds this DateTime && Duration && returns the sum as a new DateTime object.
-  DateTime operator +(Duration duration) => add(duration);
-
-  /// Subtracts the Duration from this DateTime returns the difference as a new DateTime object.
-  DateTime operator -(Duration duration) => subtract(duration);
-  //DateTime operator -=( int days)=> this.add(Duration(days:-days));
-
-  //DateTime operator +=( int days)=> this.add(Duration(days:days));
-  //DateTime operator ++()=> this.add(Duration(days:1));
-  //DateTime operator --()=>this.add(Duration(days:-1));
-
-  /// Returns a range of dates to [to], exclusive start, inclusive end
-  /// ```dart
-  /// final start = DateTime(2019);
-  /// final end = DateTime(2020);
-  /// start.to(end, by: const Duration(days: 365)).forEach(print); // 2020-01-01 00:00:00.000
-  /// ```
-  Iterable<DateTime> to(DateTime to,
-      {Duration by = const Duration(days: 1)}) sync* {
-    if (isAtSameMomentAs(to)) return;
-
-    if (isBefore(to)) {
-      var value = this + by;
-      yield value;
-
-      var count = 1;
-      while (value.isBefore(to)) {
-        value = this + (by * ++count);
-        yield value;
-      }
-    } else {
-      var value = this - by;
-      yield value;
-
-      var count = 1;
-      while (value.isAfter(to)) {
-        value = this - (by * ++count);
-        yield value;
+  DateTime toDateTime(value, {DateTime def, int z = -3}) {
+    if (value is String) {
+      var v = DateTime.tryParse(value);
+      if (v != null) {
+        int dif = (value.endsWith('Z')) ? z : 0;
+        return DateTime.tryParse(value)?.add(Duration(hours: dif));
       }
     }
+    if (value is DateTime) return value;
+    return def ?? DateTime.now();
   }
-}
-
-extension DurationTimeExtension on Duration {
-  static const int daysPerWeek = 7;
-  static const int nanosecondsPerMicrosecond = 1000;
-
-  /// Returns the representation in weeks
-  int get inWeeks => (inDays / daysPerWeek).ceil();
-
-  /// Adds the Duration to the current DateTime && returns a DateTime in the future
-  DateTime get fromNow => DateTime.now() + this;
-
-  @Deprecated('Use fromNow instead. Will be removed in 2.0.0')
-  DateTime get later => fromNow;
-
-  /// Subtracts the Duration from the current DateTime && returns a DateTime in the past
-  DateTime get ago => DateTime.now() - this;
 }

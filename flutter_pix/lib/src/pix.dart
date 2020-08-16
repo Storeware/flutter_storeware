8cd6a398eec255fa5073ac391acd443ef2de482d
User <user@Users-MacBook-Air.local>
2020-08-16 01:04:24 -0300

    =

--- flutter_pix/lib/src/pix.dart
+++ flutter_pix/lib/src/pix.dart
@@ -15,14 +15,48 @@ class PixMerchantAccount extends Pix {
   String gui;
   String chave;
   String infoAdicionais;
-  PixMerchantAccount({
+
+  ///
+  String instituicao;
+  String tipoConta;
+  String agencia;
+  String conta;
+
+  /// uso para brcode dinamico
+  String url;
+  PixBrCode brCode;
+  PixMerchantAccount(
+    this.brCode, {
     this.gui = pixGui,
     this.chave,
     this.infoAdicionais,
   });
+  validar() {
+  if (brCode.payloadFormat==PixPayloadFormat.dinamico){
+    if (instituicao==null ||  agencia==null || conta==null || tipoConta==null )
+      throw 'Dados do recebedor incompletos';
+  if (brCode.payloadFormat == PixPayloadFormat.estatico)
+    if (chave==null)
+      throw 'Nao informou a chave para transacao estatica';  
+  }
+
+  }
   @override
   String toString() {
-    return g('00', gui) + g('01', chave) + g('02', infoAdicionais);
+    validar();
+    return g('00', gui) +
+        g('01', chave.replaceAll('@', ' ').toUpperCase()) +
+        g('02', infoAdicionais) +
+        g('21', instituicao) +
+        g('22', tipoConta) +
+        g('23', agencia) +
+        g('24', conta) +
+        get72();
+  }
+
+  get72() {
+    if (url == null) return '';
+    g('72', g('00', gui) + g('25', url));
   }
 }
 
@@ -33,40 +67,94 @@ class PixPsst extends Pix {
   toString() => g('00', gui) + g('01', version);
 }
 
+class PixUnreservedTemplates extends Pix {
+  String gui;
+  String url;
+  PixUnreservedTemplates({this.gui, this.url});
+  @override
+  toString() {
+    return g('00', gui) + g('25', url);
+  }
+}
+
 class PixAdditionalDataField extends Pix {
+  /// 1.16.10 - uso interno do recebedor para identificar a transacao. Dado para fim de conciliacao.
   String txId;
   final PixPsst psst = PixPsst();
+
+  /// brcode dinamico
+  set referenceLabel(x) => txId = x;
   @override
   toString() {
     return g('05', txId) + g('50', psst.toString());
   }
 }
 
+enum PixPayloadFormat { estatico, dinamico }
+
 class PixBrCode extends Pix {
-  String payloadFormat = '01';
-  final PixMerchantAccount merchartAccount = PixMerchantAccount();
+  PixPayloadFormat payloadFormat;
+  final PixMerchantAccount merchartAccount;
   String merchantCategory;
   String transactionCurrency = '986';
   String countryCode = 'BR';
   String merchantName;
   String merchantCity;
   PixAdditionalDataField additionalDataField = PixAdditionalDataField();
+  double transactionAmount;
+  final PixUnreservedTemplates unreservedTemplates = PixUnreservedTemplates();
   String crc;
-  PixBrCode({
-    this.merchantCategory,
-    this.merchantName,
-    this.merchantCity,
-  });
+  PixBrCode(
+      {@required this.payloadFormat = PixPayloadFormat.estatico,
+      this.merchantCategory,
+      this.merchantName,
+      this.merchantCity,
+      this.transactionAmount}) {
+    merchartAccount = PixMerchantAccount(this);
+  }
+  validar() {
+    if ((merchartAccount.url ?? '').length > 77)
+      throw 'URL possui limite de 77 caracteres';
+  }
+
+  getAmout() {
+    if (transactionAmount == null) return '';
+    return g('54', transactionAmount.toStringAsFixed(2));
+  }
+
+  get payloadFmt => ['01', '02'][payloadFormat.index];
   @override
   toString() {
-    return g('01', payloadFormat) +
+    validar();
+    return g('01', payloadFmt) +
         g('26', merchartAccount.toString()) +
         g('52', this.merchantCategory) +
         g('53', this.transactionCurrency) +
         g('58', countryCode) +
+        getAmount() +
         g('59', merchantName.toUpperCase()) +
         g('60', this.merchantCity.toUpperCase()) +
         g('62', this.additionalDataField.toString().toUpperCase()) +
+        g('80', unreservedTemplates.toString()) +
         g('63', crc.toUpperCase());
   }
+}
+
+/// [PixRequest] format o pedido de transacao na URL
+// TODO:
+class PixRequest {
+  final PixBrCode brCode;
+  final String urlBase;
+  PixRequest(
+      {@required this.urlBase = 'https://pix.bcb.gov.br/qr',
+      @required this.brCode});
+  get base64 => base64Decode(brCode.toString());
+  get url => '$urlbase/$base64';
+}
+
+///[PixResponse] formata a resposta da transacao
+// TODO;
+class PixResponse {
+  final Map<String, dynamic> response;
+  PixResponse({@required this.response});
 }

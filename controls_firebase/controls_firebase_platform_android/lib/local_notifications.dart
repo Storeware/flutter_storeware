ac87902ef061605bd2649fa9c4db873c9dcf3a80
Amarildo Lacerda <amarildo_51@msn.com>
2020-09-09 15:08:54 -0300

    Update local_notifications.dart

--- controls_firebase/controls_firebase_platform_android/lib/local_notifications.dart
+++ controls_firebase/controls_firebase_platform_android/lib/local_notifications.dart
@@ -1,1 +1,18 @@
-class LocalNotifications {}
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotifications {
+
+ static  Future<void> showNotification(
+      {String title, String body, String payload}) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0, title ?? '', body ?? '', platformChannelSpecifics,
+        payload: payload ?? '');
+  }
+
+}

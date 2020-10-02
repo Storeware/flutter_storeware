c6661e9ad79f9b515eee7faab10e85071cc59f7f
Amarildo Lacerda <amarildo_51@msn.com>
2020-10-02 12:23:22 -0300

    Add files via upload

---
+++ models/lib/data/sql_builder.dart
@@ -0,0 +1,30 @@
+import 'package:controls_extensions/extensions.dart';
+
+class SqlBuilder {
+  static createSqlUpdate(collection, key, Map<String, dynamic> dados,
+      {String colunas}) {
+    var where = "$key = '${dados[key]}' ";
+    var s = '';
+    List<String> keys = dados.keys.join(',').split(',');
+    if (colunas != null) keys = colunas.split(',');
+
+    keys.forEach((k) {
+      var d = dados[k];
+      if (d == null) {
+        //s += '$k = null'; // nao enviar nulls
+      } else {
+        if (s.isNotEmpty) s += ', ';
+        if (d is num)
+          s += '$k = $d';
+        else if (d is DateTime) {
+          var dt = d.toDateTimeSql();
+
+          s += "$k = '$dt' ";
+        } else
+          s += "$k = '$d'";
+      }
+    });
+    s = 'update $collection set $s where $where';
+    return s;
+  }
+}

7bee5688d64190d19a3c69319e4b9a392a09dc1e
Amarildo Lacerda <amarildo_51@msn.com>
2020-10-14 16:54:21 -0300

    * -.html +.js

--- controls_data/lib/local_storage.dart
+++ controls_data/lib/local_storage.dart
@@ -1,7 +1,7 @@
 library local_storage;
 
 import "package:control_data_platform_android/control_data_platform_android.dart"
-    if (dart.library.html) "package:control_data_platform_web/control_data_platform_web.dart"
+    if (dart.library.js) "package:control_data_platform_web/control_data_platform_web.dart"
     if (dart.library.io) "package:control_data_platform_android/control_data_platform_android.dart"
     if (dart.library.windows) "package:control_data_platform_windows/control_data_platform_windows.dart";
 

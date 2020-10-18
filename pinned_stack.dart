9f130e83e116a73566bd20f41921edde534f24aa
Amarildo Lacerda <amarildo_51@msn.com>
2020-10-18 14:18:57 -0300

    +

---
+++ pinned_stack.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class StackConstraints extends InheritedWidget {
+  final BoxConstraints constraints;
+
+  StackConstraints({this.constraints, Widget child}) : super(child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    var old = (oldWidget as StackConstraints).constraints;
+    return old.maxWidth != constraints.maxWidth || old.maxHeight != constraints.maxHeight;
+  }
+}
+
+class PinnedStack extends StatelessWidget {
+  final List<Widget> children;
+  final StackFit fit;
+  final AlignmentGeometry alignment;
+  final TextDirection textDirection;
+  final Clip clip;
+
+  const PinnedStack(
+      {Key key,
+      this.children,
+      this.fit = StackFit.expand,
+      this.alignment = Alignment.topLeft,
+      this.textDirection = TextDirection.ltr,
+      this.clip = Clip.none})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return StackConstraints(
+        constraints: constraints,
+        child: Stack(
+          fit: fit,
+          alignment: alignment,
+          clipBehavior: clip,
+          textDirection: textDirection,
+          children: children,
+        ),
+      );
+    });
+  }
+}

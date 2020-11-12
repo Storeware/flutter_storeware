e293277dbffa92f463101207f45a7ace313191d5
Amarildo Lacerda <amarildo_51@msn.com>
2020-11-12 11:45:10 -0300

    Add files via upload

---
+++ controls_web/lib/controls/vertical_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+
+class VerticalText extends StatelessWidget {
+  final String text;
+  final TextStyle style;
+  final double runSpacing;
+  const VerticalText(
+    this.text, {
+    Key key,
+    this.style,
+    this.runSpacing = 30,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+        runSpacing: runSpacing,
+        direction: Axis.vertical,
+        alignment: WrapAlignment.center,
+        children: [
+          ...text.split("").map((string) => Text(string, style: style)),
+        ]);
+  }
+}

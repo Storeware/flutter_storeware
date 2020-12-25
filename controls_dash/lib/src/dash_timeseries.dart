de33a8f5f5ad2f2bd234ac0e4798325c033c4f3c
Amarildo Lacerda <amarildo_51@msn.com>
2020-12-24 22:07:52 -0300

    Update dash_timeseries.dart

--- controls_dash/lib/src/dash_timeseries.dart
+++ controls_dash/lib/src/dash_timeseries.dart
@@ -3,90 +3,6 @@ import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class DashTimeSeriesBar extends StatelessWidget {
-  final List<charts.Series<TimeSeriesSales, DateTime>> seriesList;
-  final bool animate;
-  final bool showSeriesNames;
-  final List<charts.SeriesRendererConfig<DateTime>> customSeriesRenderers;
-
-  DashTimeSeriesBar(this.seriesList,
-      {this.animate, this.showSeriesNames = false, this.customSeriesRenderers});
-
-  /// Creates a [TimeSeriesChart] with sample data and no transition.
-  factory DashTimeSeriesBar.withSampleData() {
-    return new DashTimeSeriesBar(
-      _createSampleData(),
-      // Disable animations for image tests.
-      animate: false,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new charts.TimeSeriesChart(
-      seriesList,
-      animate: animate,
-      // Set the default renderer to a bar renderer.
-      // This can also be one of the custom renderers of the time series chart.
-      defaultRenderer: new charts.BarRendererConfig<DateTime>(),
-      // It is recommended that default interactions be turned off if using bar
-      // renderer, because the line point highlighter is the default for time
-      // series chart.
-      defaultInteractions: false,
-      customSeriesRenderers: customSeriesRenderers,
-      // If default interactions were removed, optionally add select nearest
-      // and the domain highlighter that are typical for bar charts.
-      behaviors: [
-        new charts.SelectNearest(),
-        new charts.DomainHighlighter(),
-        if (showSeriesNames)
-          charts.SeriesLegend(position: charts.BehaviorPosition.bottom)
-      ],
-    );
-  }
-
-  /// Create one series with sample hard coded data.
-  static List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData() {
-    return createSerie(id: 'vendas', data: [
-      new TimeSeriesSales(new DateTime(2017, 9, 1), 5),
-      new TimeSeriesSales(new DateTime(2017, 9, 2), 5),
-      new TimeSeriesSales(new DateTime(2017, 9, 3), 25),
-      new TimeSeriesSales(new DateTime(2017, 9, 4), 100),
-      new TimeSeriesSales(new DateTime(2017, 9, 5), 75),
-      new TimeSeriesSales(new DateTime(2017, 9, 6), 88),
-      new TimeSeriesSales(new DateTime(2017, 9, 7), 65),
-      new TimeSeriesSales(new DateTime(2017, 9, 8), 91),
-      new TimeSeriesSales(new DateTime(2017, 9, 9), 100),
-      new TimeSeriesSales(new DateTime(2017, 9, 10), 111),
-      new TimeSeriesSales(new DateTime(2017, 9, 11), 90),
-      new TimeSeriesSales(new DateTime(2017, 9, 12), 50),
-      new TimeSeriesSales(new DateTime(2017, 9, 13), 40),
-      new TimeSeriesSales(new DateTime(2017, 9, 14), 30),
-      new TimeSeriesSales(new DateTime(2017, 9, 15), 40),
-      new TimeSeriesSales(new DateTime(2017, 9, 16), 50),
-      new TimeSeriesSales(new DateTime(2017, 9, 17), 30),
-      new TimeSeriesSales(new DateTime(2017, 9, 18), 35),
-      new TimeSeriesSales(new DateTime(2017, 9, 19), 40),
-      new TimeSeriesSales(new DateTime(2017, 9, 20), 32),
-      new TimeSeriesSales(new DateTime(2017, 9, 21), 31),
-    ]);
-  }
-
-  static List<charts.Series<TimeSeriesSales, DateTime>> createSerie(
-      {String id, List<TimeSeriesSales> data}) {
-    return [
-      new charts.Series<TimeSeriesSales, DateTime>(
-        id: id,
-        colorFn: (TimeSeriesSales sales, __) =>
-            sales.color ?? charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (TimeSeriesSales sales, _) => sales.time,
-        measureFn: (TimeSeriesSales sales, _) => sales.sales,
-        data: data,
-      )
-    ];
-  }
-}
-
 /// Sample time series data type.
 class TimeSeriesSales {
   final DateTime time;
@@ -96,25 +12,59 @@ class TimeSeriesSales {
   TimeSeriesSales(this.time, this.sales, {this.color});
 }
 
-class DashTimeSeriesLine extends StatelessWidget {
+enum DashTimeSeriesType { bar, line }
+
+class DashTimeSeries extends StatelessWidget {
   final List<charts.Series<TimeSeriesSales, DateTime>> seriesList;
   final bool animate;
   final bool showSeriesNames;
-  final List<charts.SeriesRendererConfig<DateTime>> customSeriesRenderers;
+  final DashTimeSeriesType rendererType;
+  final List<charts.SeriesRendererConfig<DateTime>> customSeriesRenderers = [];
   final bool includeArea;
   final bool stacked;
   final String dateFormat;
   final int labelRotation;
   final bool includeLine;
-  DashTimeSeriesLine(this.seriesList,
+  DashTimeSeries(this.seriesList,
       {this.animate,
-      this.customSeriesRenderers,
+      this.rendererType = DashTimeSeriesType.line,
       this.includeArea = false,
       this.stacked = false,
       this.dateFormat = 'dd/MMM',
       this.labelRotation = 0,
       this.includeLine = true,
-      this.showSeriesNames = false});
+      List<charts.SeriesRendererConfig<DateTime>> customSeriesRenderers,
+      this.showSeriesNames = false}) {
+    if (customSeriesRenderers != null)
+      this.customSeriesRenderers.addAll(customSeriesRenderers);
+  }
+
+  addSerie({
+    String id,
+    List<TimeSeriesSales> data,
+    String rendererId,
+    num strokeWidth = 1,
+    DashTimeSeriesType rendererType,
+  }) {
+    final s = createSerie(id: id, data: data, strokeWidth: strokeWidth)[0];
+    s.setAttribute(charts.rendererIdKey, rendererId ?? id);
+    seriesList.add(s);
+    if (rendererType != null) {
+      switch (rendererType) {
+        case DashTimeSeriesType.bar:
+          addSerieRenderer(
+              charts.BarRendererConfig<DateTime>(customRendererId: rendererId));
+          break;
+        default:
+          addSerieRenderer(charts.LineRendererConfig<DateTime>(
+              customRendererId: rendererId));
+      }
+    }
+    return s;
+  }
+
+  addSerieRenderer(charts.SeriesRendererConfig<DateTime> renderer) =>
+      customSeriesRenderers.add(renderer);
 
   @override
   Widget build(BuildContext context) {
@@ -123,11 +73,13 @@ class DashTimeSeriesLine extends StatelessWidget {
       animate: animate,
       // Set the default renderer to a bar renderer.
       // This can also be one of the custom renderers of the time series chart.
-      defaultRenderer: new charts.LineRendererConfig<DateTime>(
-        includeArea: includeArea,
-        stacked: stacked,
-        includeLine: includeLine,
-      ),
+      defaultRenderer: (rendererType == DashTimeSeriesType.line)
+          ? charts.LineRendererConfig<DateTime>(
+              includeArea: includeArea,
+              stacked: stacked,
+              includeLine: includeLine,
+            )
+          : charts.BarRendererConfig<DateTime>(),
       customSeriesRenderers: customSeriesRenderers,
       // It is recommended that default interactions be turned off if using bar
       // renderer, because the line point highlighter is the default for time

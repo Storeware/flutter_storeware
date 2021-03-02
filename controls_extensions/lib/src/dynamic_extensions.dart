491fd7d07de1fc011917b87a204bb77bdae018d5
Amarildo Lacerda <amarildo_51@msn.com>
2021-03-02 20:26:35 -0300

    Add files via upload

--- controls_extensions/lib/src/dynamic_extensions.dart
+++ controls_extensions/lib/src/dynamic_extensions.dart
@@ -26,15 +26,31 @@ extension DynamicExtension on dynamic {
     return def;
   }
 
-  DateTime toDateTime(value, {DateTime def, int zone = -3}) {
+  int strTimeToMinutes(String time) {
+    try {
+      var sp = time.split(':');
+      int h = 0, m = 0;
+      h = int.tryParse(sp[0]) ?? 0;
+      if (sp.length > 1) m = int.tryParse(sp[1]);
+      if (h < 0) h = h * -1;
+      num f = (time.startsWith('-')) ? -1 : 1;
+      return ((f * (h * 60) + m) ~/ 1);
+    } catch (e) {
+      return 0;
+    }
+  }
+
+  DateTime toDateTime(value, {DateTime def, num zone = -3}) {
     if (value is String) {
       var v = DateTime.tryParse(value);
-      int dif = zone;
+      num dif = zone * 60;
       if (v != null) {
-        dif = (value.endsWith('Z'))
-            ? dif
-            : 0; // quando termado Z  formatar fuso horario.
-        return DateTime.tryParse(value)?.add(Duration(hours: dif));
+        dif = (value.endsWith('Z')) ? dif : 0;
+        if ('$value'.length > 20 && v != null)
+          dif = strTimeToMinutes('$value'.substring(19));
+        // quando termado Z  formatar fuso horario.
+        return DateTime.tryParse(value)
+            ?.add(Duration(minutes: ((dif ?? 0)) ~/ 1));
       }
     }
     if (value is DateTime) return value;

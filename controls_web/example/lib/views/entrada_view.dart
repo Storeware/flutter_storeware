5263bdeec563342caf500d595f9e89484f3dd8ea
amarildolacerAda <amarildo_51@msn.com>
2021-03-15 23:29:10 -0300

    =

--- controls_web/example/lib/views/entrada_view.dart
+++ controls_web/example/lib/views/entrada_view.dart
@@ -2,6 +2,7 @@ import 'package:controls_web/controls/sliver_apps.dart';
 import 'package:app/models/apps_items.dart';
 import 'package:app/views/drawer_view.dart';
 import 'package:controls_web/controls/tab_choice.dart';
+import 'package:controls_web/controls/tabview_bottom.dart';
 import 'package:controls_web/controls/vertical_tab_view.dart';
 import 'package:controls_web/controls/vertical_toptab_navigator.dart';
 import 'package:flutter/material.dart';
@@ -15,45 +16,64 @@ class EntradaView extends StatefulWidget {
 class _EntradaViewState extends State<EntradaView> {
   @override
   Widget build(BuildContext context) {
-    return VerticalTabView(choices: [
-      TabChoice(
-        label: 'Principal',
-        enabled: true,
-        child: VerticalTopTabView(
-          completedColor: Colors.green,
-          //timeline: (x) {
-          // return Icon(Icons.check, size: 10);
-          //},
-          choices: [
-            TabChoice(
-              label: 'Opções',
-              completed: (index) {
-                return true;
-              },
-              builder: () => Scaffold(
-                drawer: Drawer(child: DrawerView()),
-                //appBar: appBarLight(
-                //  title: Text(Constantes.appName),
-                //),
-                body: SliverApps(
-                    appBar: SliverAppBar(title: Text('AppBar')),
-                    topBars: AppsItems.topBars(context),
-                    topBarsHeight: 120,
-                    body: AppsItems.body(context),
-                    grid: AppsItems.builder(context),
-                    bottomBars: AppsItems.bottom(context)),
-              ),
-            ),
-            TabChoice(label: 'Opção 2', builder: () => Text('2')),
-          ],
+    var tabB = TabViewBottom(
+      tabHeight: 70,
+      choices: [
+        TabChoice(
+          title: Text('show1'),
+          onPressed: () {},
+          child: Container(),
+          //icon: Icons.ac_unit_outlined,
+          image: Text('1'),
+          //builder: () => Text('x'),
         ),
-      ),
-      TabChoice(
-          label: 'Outros',
-          // child: Container(child: Text('pagina outros')),
-          builder: () {
-            return Text('Outros');
-          })
-    ]);
+        TabChoice(
+          title: Text('show2'),
+          builder: () => Text('x'),
+        ),
+      ],
+    );
+    return VerticalTabView(
+        /*bottomNavigationBar: ,*/
+        choices: [
+          TabChoice(
+            label: 'Principal',
+            enabled: true,
+            child: VerticalTopTabView(
+              completedColor: Colors.green,
+              //timeline: (x) {
+              // return Icon(Icons.check, size: 10);
+              //},
+              choices: [
+                TabChoice(
+                  label: 'Opções',
+                  completed: (index) {
+                    return true;
+                  },
+                  builder: () => Scaffold(
+                    drawer: Drawer(child: DrawerView()),
+                    //appBar: appBarLight(
+                    //  title: Text(Constantes.appName),
+                    //),
+                    body: SliverApps(
+                        appBar: SliverAppBar(title: Text('AppBar')),
+                        topBars: AppsItems.topBars(context),
+                        topBarsHeight: 120,
+                        body: AppsItems.body(context),
+                        grid: AppsItems.builder(context),
+                        bottomBars: AppsItems.bottom(context)),
+                  ),
+                ),
+                TabChoice(label: 'Opção 2', builder: () => Text('2')),
+              ],
+            ),
+          ),
+          TabChoice(
+              label: 'Outros',
+              // child: Container(child: Text('pagina outros')),
+              builder: () {
+                return Text('Outros');
+              })
+        ]);
   }
 }

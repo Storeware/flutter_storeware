56c2add022f11fd75acfbdc587c247da5ede426a
amarildolacerAda <amarildo_51@msn.com>
2021-03-30 12:38:51 -0300

    * nulls

--- controls_web/lib/controls/background_widget.dart
+++ controls_web/lib/controls/background_widget.dart
@@ -1,3 +1,4 @@
+// @dart=2.12
 //import 'package:checkout/app/config/loja_config.dart';
 import 'package:flutter/material.dart';
 
@@ -16,16 +17,21 @@ class BackgroundWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //return Container();
     return Stack(
       children: <Widget>[
         if (topNavigationBar != null) topNavigationBar!,
         Positioned(
           bottom: 1,
-          child: image!, //?? LojaConfig.backgroundImage,
+          child: (image == null)
+              ? Container()
+              : image ?? Container(), //?? LojaConfig.backgroundImage,
         ),
         if (bottomNavigationBar != null)
-          Align(alignment: Alignment.bottomCenter, child: bottomNavigationBar),
-        child!,
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: bottomNavigationBar ?? Container()),
+        if (child != null) child!,
       ],
     );
   }

65af0a11536274373bb1964788c369e8e7ef8b7f
Amarildo Lacerda <amarildo_51@msn.com>
2021-05-07 18:53:52 -0300

    - extensions

--- controls_data/lib/odata_client.dart
+++ controls_data/lib/odata_client.dart
@@ -48,60 +48,60 @@ extension MapExtension on Map {
   keysJoin({String separator = ','}) => this.keys.join(separator);
 }
 
-extension DynamicExtension on dynamic {
-  int toInt(value, {def = 0}) {
-    if (value is int) return value;
-    if (value is num) return value.toInt();
-    if (value is String) return int.tryParse(value) ?? def;
-    return def;
-  }
-
-  double toDouble(value, {def = 0.0}) {
-    if (value is double) return value;
-    if (value is int) return value + 0.0;
-    if (value is num) return value + 0.0;
-    if (value is String)
-      return double.tryParse(value.replaceAll(',', '.')) ?? def;
-    return def;
-  }
-
-  bool toBool(value, {def: false}) {
-    if (value is bool) return value;
-    if (value is num) return (value == 0) ? false : true;
-    if (value is String) return (value == '1' || value == 'T');
-    return def;
-  }
-
-  DateTime? toDateTime(value, {DateTime? def, zone = 0}) {
-    if (value is String) {
-      int dif = (value.endsWith('Z') ? zone : 0);
-      return DateTime.tryParse(value)?.add(Duration(hours: dif)) ?? def;
-    }
-    if (value is DateTime) return value;
-    return def ?? DateTime.now();
-  }
-
-  DateTime? toDate(value, {DateTime? def}) {
-    if (value is String) {
-      def = DateTime.tryParse(value.substring(0, 10)) ?? DateTime.now();
-    }
-    if (value is DateTime) def = value;
-    def ??= DateTime.now();
-    return DateTime(def.year, def.month, def.day);
-  }
-
-  String toTimeSql(DateTime value) {
-    return value.toIso8601String().substring(11, 19);
-  }
-
-  String toDateTimeSql(DateTime value) {
-    return value.toIso8601String().substring(0, 19).replaceAll('T', ' ');
-  }
-
-  String toDateSql(DateTime value) {
-    return value.toIso8601String().substring(0, 10);
-  }
+//extension DynamicExtension on dynamic {
+int toInt(value, {def = 0}) {
+  if (value is int) return value;
+  if (value is num) return value.toInt();
+  if (value is String) return int.tryParse(value) ?? def;
+  return def;
 }
+
+double toDouble(value, {def = 0.0}) {
+  if (value is double) return value;
+  if (value is int) return value + 0.0;
+  if (value is num) return value + 0.0;
+  if (value is String)
+    return double.tryParse(value.replaceAll(',', '.')) ?? def;
+  return def;
+}
+
+bool toBool(value, {def: false}) {
+  if (value is bool) return value;
+  if (value is num) return (value == 0) ? false : true;
+  if (value is String) return (value == '1' || value == 'T');
+  return def;
+}
+
+DateTime? toDateTime(value, {DateTime? def, zone = 0}) {
+  if (value is String) {
+    int dif = (value.endsWith('Z') ? zone : 0);
+    return DateTime.tryParse(value)?.add(Duration(hours: dif)) ?? def;
+  }
+  if (value is DateTime) return value;
+  return def ?? DateTime.now();
+}
+
+DateTime? toDate(value, {DateTime? def}) {
+  if (value is String) {
+    def = DateTime.tryParse(value.substring(0, 10)) ?? DateTime.now();
+  }
+  if (value is DateTime) def = value;
+  def ??= DateTime.now();
+  return DateTime(def.year, def.month, def.day);
+}
+
+String toTimeSql(DateTime value) {
+  return value.toIso8601String().substring(11, 19);
+}
+
+String toDateTimeSql(DateTime value) {
+  return value.toIso8601String().substring(0, 19).replaceAll('T', ' ');
+}
+
+String toDateSql(DateTime value) {
+  return value.toIso8601String().substring(0, 10);
+}
+//}
 
 class LoginTokenChanged extends BlocModelX<bool> {
   static final _singleton = LoginTokenChanged._create();

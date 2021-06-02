303ccbe78471544d1c7cc901030de85397f5e81f
Amarildo Lacerda <amarildo_51@msn.com>
2021-06-02 14:16:49 -0300

    = compatibilidade

--- controls_firebase/controls_firebase_platform_web/lib/controls_firebase_platform_web.dart
+++ controls_firebase/controls_firebase_platform_web/lib/controls_firebase_platform_web.dart
@@ -1,8 +1,8 @@
 library controls_firebase_platform_web;
 
 import 'dart:async';
-import 'dart:io';
-
+import 'dart:io' as io;
+//import 'package:universal_io/io.dart' as io;
 import 'dart:typed_data';
 // ignore_for_file:
 import 'package:controls_firebase_platform_interface/controls_firebase_platform_interface.dart';
@@ -145,9 +145,10 @@ class FirebaseStorageDriver extends FirebaseStorageDriverInterface {
   Future<int> uploadFileImage(String path, rawPath, {metadata}) async {
     String _fileName = buildPath(path);
     fs.Reference firebaseStorageRef = storage.ref().child(_fileName);
-    print('$_fileName:$rawPath');
-    final Directory systemTempDir = Directory.systemTemp;
-    final File file = await new File('${systemTempDir.path}/temp.jpg').create();
+    //print('$_fileName:$rawPath');
+    final io.Directory systemTempDir = io.Directory.systemTemp;
+    final io.File file =
+        await io.File('${systemTempDir.path}/temp.jpg').create();
     file.writeAsBytes(rawPath);
     final md;
     md = fs.SettableMetadata(

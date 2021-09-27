f74f34402b3a7b51672d62049002c79afc497853
amarildolacerAda <amarildo_51@msn.com>
2021-09-27 19:49:35 -0300

    =

--- controls_image/lib/controls_image.dart
+++ controls_image/lib/controls_image.dart
@@ -3,4 +3,8 @@ library controls_image;
 import 'package:controls_image_android/controls_image_android.dart'
     if (dart.library.js) 'package:controls_image_web/controls_image_web.dart';
 
+
 class ImagePicker extends ControlsImage {}
+
+
+

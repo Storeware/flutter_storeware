24e281cf02123ff61e23c63b3d42d7f9e404f8c9
amarildolacerAda <amarildo_51@msn.com>
2021-10-04 08:04:40 -0300

    =

---
+++ controls_extensions/lib/src/widget_extensions.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+// Our design contains Neumorphism design and i made a extention for it
+// We can apply it on any  widget
+
+extension Neumorphism on Widget {
+  addNeumorphism({
+    double borderRadius = 10.0,
+    Offset offset = const Offset(5, 5),
+    double blurRadius = 10,
+    Color topShadowColor = Colors.white60,
+    Color bottomShadowColor = const Color(0x26234395),
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+        boxShadow: [
+          BoxShadow(
+            offset: offset,
+            blurRadius: blurRadius,
+            color: bottomShadowColor,
+          ),
+          BoxShadow(
+            offset: Offset(-offset.dx, -offset.dx),
+            blurRadius: blurRadius,
+            color: topShadowColor,
+          ),
+        ],
+      ),
+      child: this,
+    );
+  }
+}import 'package:flutter/material.dart';
+
+// Our design contains Neumorphism design and i made a extention for it
+// We can apply it on any  widget
+
+extension WidgetMorphism on Widget {
+  shadow({
+    double borderRadius = 10.0,
+    Offset offset = const Offset(5, 5),
+    double blurRadius = 10,
+    Color topShadowColor = Colors.white60,
+    Color bottomShadowColor = const Color(0x26234395),
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
+        boxShadow: [
+          BoxShadow(
+            offset: offset,
+            blurRadius: blurRadius,
+            color: bottomShadowColor,
+          ),
+          BoxShadow(
+            offset: Offset(-offset.dx, -offset.dx),
+            blurRadius: blurRadius,
+            color: topShadowColor,
+          ),
+        ],
+      ),
+      child: this,
+    );
+  }
+  paddingAll(double all=8.0){
+    return Padding(
+      padding:EdgeInsets.all(all),
+      child:this;
+    )
+  }
+  card({Color? color,double elevation=4.0}){
+    return Card(
+      color:color,
+      elevation:elevation,
+      child:this,
+    )
+  }
+  sizedBox({Color? color,double? height, double? width,}){
+    return SizedBox(color:color,height:height,width:width,child:this);
+  }
+}

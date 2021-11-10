e7cd5826be5ad86a97d366d0c3385b59bdd2cf72
amarildolacerAda <amarildo_51@msn.com>
2021-11-10 11:22:10 -0300

    = rename package

--- odata_sqlite/test/odata_sqlite_test.dart
+++ odata_sqlite/test/odata_sqlite_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:odata_sqlite/odata_sqlite.dart';
 
 void main() {
-  test('adds one to input values', () {
+ /* test('adds one to input values', () {
     final calculator = Calculator();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
-  });
+  });*/
 }

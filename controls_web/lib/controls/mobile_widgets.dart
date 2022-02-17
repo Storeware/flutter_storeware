0bfaa6a9c7f5946bc9f7038f529883e397b46fa0
Amarildo de Lacerda <amarildo_51@msn.com>
2022-02-16 21:06:14 -0300

    = check nullsave onpressed

--- controls_web/lib/controls/mobile_widgets.dart
+++ controls_web/lib/controls/mobile_widgets.dart
@@ -120,6 +120,10 @@ class MobileMenuBox extends StatelessWidget {
                 color: color,
                 child: InkButton(
                   onTap: () {
+                    if (item.onPressed != null) {
+                      item.onPressed!();
+                      return;
+                    }
                     Get.to(
                       () => (item.primary)
                           ? Scaffold(
@@ -141,7 +145,10 @@ class MobileMenuBox extends StatelessWidget {
                                                 InkWell(
                                                     child: sb.image,
                                                     onTap: () {
-                                                      sb.onPressed!();
+                                                      if (sb.onPressed !=
+                                                          null) {
+                                                        sb.onPressed!();
+                                                      }
                                                     })
                                             ]
                                           : null,

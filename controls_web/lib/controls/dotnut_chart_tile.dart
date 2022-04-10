78ffd1398a5399175a3b3fff515478a4bb20a0bd
amarildo de lacerda <amarildo_51@msn.com>
2022-04-09 21:08:49 -0300

    + dotnut_chart_tile

---
+++ controls_web/lib/controls/dotnut_chart_tile.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class DotnutChartTile extends StatefulWidget {
+  const DotnutChartTile({
+    Key? key,
+    this.height = 140,
+    this.width = 320,
+    this.value,
+    this.total,
+    this.percent,
+    this.title,
+    this.subtitle,
+    this.icon,
+    this.color,
+    this.onPressed,
+  }) : super(key: key);
+  final double? height;
+  final double? width;
+  final String? value;
+  final double? total;
+  final double? percent;
+  final Widget? title;
+  final Widget? subtitle;
+  final IconData? icon;
+  final Color? color;
+  final Function()? onPressed;
+  @override
+  _DotnutChartTileState createState() => _DotnutChartTileState();
+}
+
+class _DotnutChartTileState extends State<DotnutChartTile> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return (widget.onPressed == null)
+        ? _builder(theme)
+        : InkWell(
+            child: _builder(theme),
+            onTap: widget.onPressed!,
+          );
+  }
+
+  Card _builder(ThemeData theme) {
+    return Card(
+      color: widget.color,
+      child: SizedBox(
+        height: widget.height,
+        width: widget.width,
+        child: Row(children: [
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (widget.icon != null)
+                  CircleAvatar(
+                    child: Icon(
+                      widget.icon,
+                    ),
+                  ),
+                const SizedBox(
+                  height: 5,
+                ),
+                if (widget.title != null)
+                  DefaultTextStyle(
+                      style: theme.textTheme.bodyText1!.copyWith(
+                        fontSize: 16,
+                      ),
+                      child: widget.title!),
+                const SizedBox(
+                  height: 5,
+                ),
+                if (widget.value != null)
+                  Text(
+                    widget.value!,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                if (widget.total != null) Text('${widget.total!}'),
+                const Spacer(),
+                if (widget.subtitle != null)
+                  DefaultTextStyle(
+                      child: widget.subtitle!,
+                      style: theme.textTheme.bodyText2!.copyWith(
+                        fontSize: 10,
+                      )),
+              ],
+            ),
+          )),
+          widget.percent == null
+              ? Container()
+              : SizedBox(
+                  height: widget.height! * 0.8,
+                  width: widget.height! * 0.8,
+                  child: Center(
+                    child: Stack(
+                      children: [
+                        Center(
+                          child: SizedBox(
+                            height: widget.height! * fator,
+                            width: widget.height! * fator,
+                            child: CircularProgressIndicator(
+                              semanticsValue: '${widget.value}%',
+                              strokeWidth: 15,
+                              value: widget.percent! / 100,
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Text('${widget.percent!.toStringAsFixed(0)}%',
+                              style: const TextStyle(
+                                fontSize: 9,
+                              )),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+        ]),
+      ),
+    );
+  }
+
+  double get fator => 0.5;
+}
--- example/lib/mix4_view.dart
+++ example/lib/mix4_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_storeware/index.dart';
 
+import 'dotnut_chart_tile.dart';
+
 class Mix4View extends StatelessWidget {
   const Mix4View({Key? key}) : super(key: key);
 
@@ -8,7 +10,20 @@ class Mix4View extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [],
+        children: [
+          DotnutChartTile(
+            color: Colors.amber,
+            title: const Text('Total Expenses'),
+            value: 'R\$ 14,160',
+            percent: 62,
+            subtitle: const Text('nas ultimas 24 horas'),
+            icon: Icons.dashboard,
+            onPressed: () {
+              Dialogs.info(context, text: 'DotnutChartTile.onPressed');
+            },
+          ),
+          const DotnutChartTile()
+        ],
       ).singleChildScrollView(),
     );
   }

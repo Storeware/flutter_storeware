8c0245bde0b8b4cf2a89ed5ae79849b2eea52f8e
amarildo de lacerda <amarildo_51@msn.com>
2022-04-09 23:09:33 -0300

    + score

--- example/lib/mix4_view.dart
+++ example/lib/mix4_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_storeware/index.dart';
 
+import '../../controls_web/lib/controls/dashboard_score.dart';
+
 class Mix4View extends StatelessWidget {
   const Mix4View({Key? key}) : super(key: key);
 
@@ -28,36 +30,70 @@ class Mix4View extends StatelessWidget {
             icon: Icons.air_sharp,
             subtitle: Text('agosto/2021'),
           ),
-          const DotnutChartTile(
+          /* const DotnutChartTile(
             percent: 52,
             elevation: 10,
             child: Text('child sample'),
             subtitle: Text('OK    '),
             title: Text('Faturamento'),
             value: 'BRL 10203.2',
+          ),*/
+          SizedBox(
+            height: 120,
+            child: Row(
+              children: const [
+                DashboardTile(
+                  maxWidth: 180,
+                  title: 'DashboardTile',
+                  counter: Text(' 123 '),
+                  avatarColor: Colors.amber,
+                  color: Colors.indigo,
+                  value: '10',
+                  avatarChild: Text('99'),
+                  image: Icon(Icons.headphones),
+                ),
+                DashboardTile(
+                  maxWidth: 180,
+                  title: 'DashboardTile',
+                  avatarColor: Colors.amber,
+                  color: Colors.amber,
+                  value: '\$ 10,34',
+                  image: Icon(Icons.headphones),
+                ),
+              ],
+            ),
           ),
           Row(
-            children: const [
-              DashboardTile(
-                maxWidth: 180,
-                title: 'DashboardTile',
-                counter: Text(' 123 '),
-                avatarColor: Colors.amber,
-                color: Colors.indigo,
-                value: '10',
-                avatarChild: Text('99'),
-                image: Icon(Icons.headphones),
+            children: [
+              DashboardScore(
+                color: Colors.blue[50],
+                icon: const Icon(Icons.table_bar),
+                score: '\$ 12,234',
+                width: 180,
+                tagColor: Colors.amber,
+                title: const Text('DashboardScore'),
               ),
-              DashboardTile(
-                maxWidth: 180,
-                title: 'DashboardTile',
-                avatarColor: Colors.amber,
-                color: Colors.amber,
-                value: '\$ 10,34',
-                image: Icon(Icons.headphones),
-              ),
+              DashboardScore(
+                position: TagPosition.bottom,
+                color: Colors.amber[50],
+                //icon: const Icon(Icons.table_chart, color: Colors.red),
+                //score: '\$ 12',
+                width: 180,
+                tagColor: Colors.blue,
+                title: const Text('12.145',
+                    style: TextStyle(
+                      fontSize: 18,
+                    )),
+                child: const Text('Revendas',
+                    style: TextStyle(
+                      fontSize: 11,
+                    )),
+                onPressed: () {
+                  Dialogs.info(context, text: 'DashboardScore.onPressed');
+                },
+              )
             ],
-          ),
+          )
         ],
       ).singleChildScrollView(),
     );
